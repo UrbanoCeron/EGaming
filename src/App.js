361219fdc246684019166d348d262bae283b3dd8
Dev <dev@gmail.com>
2021-02-17 22:31:13 -0600

    Componente searchbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //import './App.sass';
 
 import Header from "./views/header";
+import Searchbar from "./views/Searchbar";
 import TopOffers from "./views/TopOffers";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     return (
         <div>
             <TopOffers title={a}></TopOffers>
-            <Header></Header>
+            <Searchbar info={"Esta la info por destructuring"}></Searchbar>
         </div>
 );
 }
---
+++ src/views/Searchbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Componente por clase
+// class Searchbar extends React.Component{
+//     render() {
+//         return <div>:D</div>
+//     } 
+// }
+
+// Componente por funcion
+const Searchbar = ({info}) => {
+    return <h1>{info}</h1>
+}
+ 
+
+
+export default Searchbar;
