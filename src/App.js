9e26ffd41e3775719d5aa0194d5304c8e355509e
Urbano Ceron <uceron.develop@gmail.com>
2021-02-24 22:57:08 -0600

    Integracion cardSection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Firebase from "./config-fb/config";
 import Searchbar from "./views/Searchbar";
 import TopOfFer from "./views/TopOffer/TopOffer"
@@ -7,27 +7,24 @@ import CardSection from "./Components/CardSection/CardSection"
 
 
 const App = () => {
-    let a = "Hasta 15% de <span style='color: #00d1b2;'>Descuento diferentes categorias</span> en <br /> DIFERENTES CATEGORIAS";
+    // let a = "Hasta 15% de <span style='color: #00d1b2;'>Descuento diferentes categorias</span> en <br /> DIFERENTES CATEGORIAS";
 
-    useEffect( () => {
-        const wordRef = Firebase.database().ref('cards');
-        wordRef.on('value', (item) => {
-            console.log(item.val());
-        });
-    },[])
+    // const [state,setState] = useState();
+
+    // useEffect( () => {
+    //     const wordRef = Firebase.database().ref('cards');
+    //     wordRef.on('value', (item) => {
+    //         debugger
+    //         setState(item.val());
+    //         console.log(item.val());
+    //     });
+    // },[])
 
     return (
         <div>
-            <h1>Este es un conflicto</h1>
-            <TopOfFer title={a}></TopOfFer>
-            <Searchbar info={"Esta la info por destructuring"}></Searchbar>
-            <p>
-                loremaldkjasdkjalkdjaldjlakdjladjaljds
-            </p>
-            <h1> Hola mundo</h1> 
-
-            <CardSection></CardSection>
-
+            {/* { 
+                state && state.map( (item,i) => <CardSection key={i} {...item} ></CardSection>  )
+            } */}
         </div>
         
 );
--- src/Components/CardSection/CardSection.jsx
+++ src/Components/CardSection/CardSection.jsx
@@ -1,5 +1,22 @@
 import React from "react";
-const CardSection = () => {
-    return <div>Mi primer componente</div>
+import styles from "./CardSection.module.css"
+
+const CardSection = ({
+    brand, producto, section
+}) => {
+    return <div className={styles.container}>
+        <div className={styles.cardContainer}>
+            <div className={styles.header}>
+                <a href="#">
+                    <img src="" alt="" />
+                </a>
+                <h2>{brand}</h2>
+                <h4>{section}</h4>
+            </div>
+            <div className={styles.descripcion}>
+                <p>{producto}</p>
+            </div>
+        </div>
+    </div>
 }
 export default CardSection;
