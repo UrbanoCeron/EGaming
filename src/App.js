117ad5dcae7274f9f72ed16f69ad42e540205a6d
Urbano Ceron <uceron.develop@gmail.com>
2021-02-24 22:37:26 -0600

    Se importa el componente CardSection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Searchbar from "./views/Searchbar";
 import TopOfFer from "./views/TopOffer/TopOffer"
+import CardSection from "./Components/CardSection/CardSection"
 
 function App() {
     let a = "Hasta 15% de <span style='color: #00d1b2;'>Descuento diferentes categorias</span> en <br /> DIFERENTES CATEGORIAS";
